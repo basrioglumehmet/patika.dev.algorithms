de9d6716c8ba57ec3a0533f50b1dc8f1df37f2d5
basrioglumehmet <basrioglumehmet@gmail.com>
2024-06-04 10:15:02 +0300

    first commit

---
+++ patikadevsortalgorithmcase/InsertionSort/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+{
+    int[] numbers = { 22, 27, 16, 2, 18, 6 };
+
+    /*
+    * 22,27,16,2,18,6 Sırasız dizi
+    * 22,16,27,2,18,6 27 ile 16 yer değişir
+    * 16,22,27,2,18,6 16 ile 22 yer değişir (22 büyük)
+    * 16,22,2,27,18,6 2 ile 27 yer değiştirir. (27 büyük)
+    * 16,2,22,27,18,6 22 ile 2 yer değiştirir
+    * 2,16,22,27,18,6 16 ile 2 yer değiştirir
+    * 2,16,22,18,27,6 27 ile 18 yer değiştirir (27 büyük)
+    * 2,16,18,22,27,6 22 ile 18 yer değiştirir
+    * 2,16,18,22,6,27 6 ile 27 yer değiştrir ve bu 6'dan küçük eleman bulana kadar devam eder
+    * 2,6,16,18,22,27 son hali olur.
+
+    * Average case: Aradığımız sayının ortada olması
+    * Worst case: Aradığımız sayının sonda olması
+    * Best case: Aradığımız sayının dizinin en başında olması.
+
+    * [22,27,16,2,18,6] -> Insertion Sort
+    * Yukarı verilen dizinin sort türüne göre aşamalarını yazınız.
+    * Big-O gösterimi: N kadar işlem için başka n kadar işlem yapılıyor. O(N^2) dominant
+    * Time Complexity: Dizi sıralandıktan sonra 18 sayısı aşağıdaki case'lerden hangisinin kapsamına girer? Average Case çünkü aradığımız sayı dizinin ortasına gelmektedir. 
+    */
+    SortByInsertion(numbers);
+    Console.WriteLine("Sıralanmış dizi: " + string.Join(",", numbers));
+}
+static void SortByInsertion<T>(T[] array) where T : IComparable<T>
+{
+    //Adım 1: Her zaman dizinin ikinci elemanından başlanır (i = 1)
+    for (int i = 1; i < array.Length; i++)
+    {
+        // current: sıralamak istediğimiz mevcut eleman
+        T current = array[i];
+        // before: current elemanının bir önceki indeksi
+        int before = i - 1;
+        /*
+         * CompareTo metodu şu sonuçları verir:
+         * Negatif bir sayı: Eğer current, array[before]'dan küçükse.
+         * Sıfır: Eğer current, array[before]'a eşitse.
+         * Pozitif bir sayı: Eğer current, array[before]'dan büyükse.
+         */
+        //Adım 2: current elemanı, solundaki elemanlarla karşılaştırılır
+        while (before >= 0 && array[before].CompareTo(current) > 0)
+        {
+            //Adım 3: Eğer current, array[before]'dan küçükse, array[before]'ı bir sağa kaydırır
+            array[before + 1] = array[before];
+            //Adım 4: Bir sonraki sol elemanı kontrol etmek için before indeksini bir azaltır
+            before--;
+        }
+        //Adım 5: current elemanını doğru konumuna yerleştirir
+        array[before + 1] = current;
+    }
+}
---
+++ patikadevsortalgorithmcase/SelectionSort/Program.cs
@@ -0,0 +1,51 @@
+﻿{
+    int[] numbers = { 7, 3, 5, 8, 2, 9, 4, 15, 6 };
+    SortBySelection(numbers);
+    Console.WriteLine("Sıralanmış dizi: " + string.Join(",", numbers));
+}
+/*
+1. Adim: { 2, 3, 5, 8, 7, 9, 4, 15, 6}
+2. Adim: { 2, 3, 5, 8, 7, 9, 4, 15, 6}
+3. Adim: { 2, 3, 4, 8, 7, 9, 5, 15, 6}
+4. Adim: { 2, 3, 4, 5, 7, 9, 8, 15, 6}
+5. Adim: { 2, 3, 4, 5, 6, 9, 8, 15, 7}
+6. Adim: { 2, 3, 4, 5, 6, 7, 8, 15, 9}
+7. Adim: { 2, 3, 4, 5, 6, 7, 8, 15, 9}
+8. Adim: { 2, 3, 4, 5, 6, 7, 8, 9, 15}
+9. Adim: { 2, 3, 4, 5, 6, 7, 8, 9, 15}
+Siralanmis dizi: 2,3,4,5,6,7,8,9,15
+*/
+
+// Her n kadar dönerken başka bir n kadar döndük bu'da Big O(N^2) yapar. Worst Case ve Maliyetli.
+//Comprable koyma nedenimiz karşılaştırma yapacağımız içindir.
+static void SortBySelection<T>(T[] array) where T : IComparable
+{
+    //Eleman başına işlem tekrarı
+    for (int a = 0; a < array.Length; a++)
+    {
+        int min = a; //Varsayılan sayı
+        for (int b = a + 1; b < array.Length; b++)
+        {
+            // Minimum olarak varsayılan sayıyla sonraki sayıları kıyasla ve başka bir minimum sayı görürsen yeni düşük sayıyı setle
+            if (array[min].CompareTo(array[b]) > 0)
+            {
+                min = b;
+            }
+        }
+        var tmp = array[a]; //Geçici olarak a elemanı sakla
+        array[a] = array[min]; //En küçük sayıyı en büyük sayıyla değiştir
+        array[min] = tmp; //Eski küçük sayının indeks değerini büyük sayıyla değiştir.
+
+        Console.Write($"{a + 1}. Adım: {"{"} ");
+        for (int k = 0; k < array.Length; k++)
+        {
+            Console.Write(array[k]);
+            if (k < array.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.Write($"{"}"}");
+        Console.WriteLine();
+    }
+}
